500e8b92feaea78fe2c1bc2d491f29ad2edc8662
Anna Mosley <annahmosley@gmail.com>
2020-12-23 08:27:23 -0500

    updated code need to add collision feature

--- js/index.js
+++ js/index.js
@@ -1,66 +1,29 @@
-const cl = (...p) => console.log(...p);
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
 window.onload = () => {
-  document.getElementById("start-button").onclick = () => {
+  document.getElementById('start-button').onclick = () => {
     startGame();
   };
+
+  
   function startGame() {
-    drawingLoop()
+    const canvas = document.getElementById('canvas');
+    const context = this.canvas.getContext('2d');
+    const game = new Game(canvas, context);
+    game.init();
+
+
+    document.addEventListener('keydown', event => {
+      switch(event.code) {
+        case 'ArrowLeft':
+        case 'KeyA':
+          if(game.car.x > 55) game.car.x -= 20; 
+          break;
+        case 'ArrowRight':
+        case 'KeyD':
+          if(game.car.x <= game.canvas.width - game.car.width - 55) game.car.x += 20;
+          break;
+      };
+      console.log(game.car.x);
+    });
   }
-};
-
-const score= 0
-const roadImg= new Image();
-roadImg.src='../images/road.png'
-const road= {
-  x:0,
-  y:0,
-  width: canvas.width,
-  height:canvas.height,
-};
-
-const carImg = new Image();
-carImg.src='../images/car.png'
-const car = {
-  x: 240,
-  y: 600,
-  width: 35,
-  height: 80,
-}
-
-function drawEverything() {
-  context.drawImage(roadImg, road.x, road.y, road.width, road.height);
-
-  context.fillStyle = 'white';
-  context.font = '20px Arial';
-  context.fillText(`Score: ${score}`, 75, 695);
-
-  context.drawImage(carImg, car.x, car.y, car.width, car.height);
-}
-document.addEventListener("keydown", (event) => {
-  switch (event.code) {
-    case "ArrowLeft":
-    case "KeyA":
-      cl("arrow left");
-      if (car.x >= 45) car.x -= 10;
-      break;
-    case "ArrowRight":
-    case "KeyD":
-      cl("arrow right");
-      if (car.x <= canvas.width - car.width - 45) car.x += 10;
-      break;
-    default:
-      console.log("You can use only arrows and ASDW!");
-  }
-});
-const clearCanvas = () => {
-  context.clearRect(0,0, canvas.width, canvas.height);
-};
-
-const drawingLoop = () => {
-  clearCanvas()
-  drawEverything()
-  requestAnimationFrame(drawingLoop);
 };
 
